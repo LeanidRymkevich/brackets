79344090cbab893bf6a5c28bf7971d50d5fe39c9
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-01-22 23:55:23 +0300

    feat: adding a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0){
+    return false;
+  }
+
+  for(let item of bracketsConfig){
+    if(!oneTypeBrackets(str, item)){
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+function oneTypeBrackets(string, source) {
+  let black_list = [];
+  
+  for(let i = 0; i < string.length; i++){
+
+    if(black_list.find(item => i === item)){
+      continue;
+    }
+
+    let isCorrect = false;
+
+    if(string[i] === source[0]){
+
+      for(let j = i + 1 ; j < string.length; j += 2){
+
+        
+        if(black_list.find(item => j === item)){
+          continue;
+        }
+
+        if(string[j] === source[1]){
+          isCorrect = true;
+          black_list.push(j);
+          break;
+        }
+      }
+
+    } else {
+      continue;
+    }
+
+    if(!isCorrect){
+      return false;
+    }
+  }
+
+  return true;
 }
